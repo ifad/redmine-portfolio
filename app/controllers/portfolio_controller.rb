1140d30a3e0c16cc4377502681616acd9d17ba8a
Lleïr Borràs Metje <lleir@llegeix.me>
2014-03-07 08:53:29 +0100

    Avoid public_access to be cached on the controller

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -3,11 +3,21 @@ class PortfolioController < ApplicationController
 
   layout 'portfolio'
 
-  if Portfolio::Redmine.public_access?
-    skip_before_filter :user_setup, :check_if_login_required, :check_password_change, :only => :index
-  end
+  PRIVATE_ACCESS = [
+    :user_setup,
+    :check_if_login_required,
+    :check_password_change
+  ]
+
+  skip_before_filter *PRIVATE_ACCESS, :only => :index
+  before_filter :public_access?, :only => :index
 
   def index
     @projects = Project.portfolio
   end
+
+  protected
+    def public_access?
+      PRIVATE_ACCESS.map{ |v| send(v) }.all? unless Portfolio::Redmine.public_access?
+    end
 end
