9df3a5472c9ae861bad9c510d0a0287c9bfd8bd6
Lleïr Borràs Metje <lleir@llegeix.me>
2014-03-10 14:58:49 +0100

    Cleanup

--- app/helpers/portfolio_helper.rb
+++ app/helpers/portfolio_helper.rb
@@ -1,25 +1,4 @@
 module PortfolioHelper
-  def portfolio_link_to_project(project, options = {}, &block)
-    options[:target] = :blank
-
-    if block_given?
-      link_to project.homepage.presence || '#', options, &block
-    else
-      link_to project.portfolio_name, project.homepage.presence || '#', options
-    end
-  end
-
-  def portfolio_link_to_modal(project, options = {}, &block)
-    options[:class] = "#{options[:class]} info"
-    (options[:data] ||= {}).merge!({
-      :image => portfolio_image_for(project).to_str,
-      :url => project.homepage,
-      :description => textilizable(project.short_description, :project => project).to_str,
-      :title => portfolio_link_to_project(project, :class => 'homepage').to_str
-    })
-
-    link_to '#', options, &block
-  end
 
   def portfolio_image_src_for(project)
     if project.portfolio_image =~ /\/attachments\/download\/(\d+)\/.+\.(\w+)$/i
@@ -36,4 +15,5 @@ module PortfolioHelper
   def mobile_info_toggler
     link_to image_tag('info.png', :plugin => 'portfolio'), "#", :id => 'mobile-info-toggler', :data => { :active => 'info_active.png', :inactive => 'info.png'}
   end
+
 end
