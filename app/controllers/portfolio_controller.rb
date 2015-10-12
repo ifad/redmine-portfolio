3d88c55560f8905a7d131d829add6c4dfa2aa000
Lleïr Borràs Metje <lleir@llegeix.me>
2015-10-12 16:13:24 +0200

    Re enable anonymous mode

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -3,10 +3,24 @@ class PortfolioController < ApplicationController
 
   layout 'portfolio'
 
+  PRIVATE_ACCESS = [
+    :user_setup,
+    :check_if_login_required,
+    :check_password_change
+  ]
+
+  skip_before_filter *PRIVATE_ACCESS, :only => :index
+  before_filter :public_access?, :only => :index
+
   helper :all
 
   def index
     @projects = Project.portfolio
   end
 
-end:q
+  protected
+    def public_access?
+      PRIVATE_ACCESS.map{ |v| send(v) }.all? unless Portfolio::Redmine.public_access?
+    end
+
+end
