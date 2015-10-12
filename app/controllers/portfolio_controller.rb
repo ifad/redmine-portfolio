81cc1eb72a086f11fa7a33279c3f870b4b98ca77
Lleïr Borràs Metje <lleir@llegeix.me>
2015-10-12 15:18:36 +0200

    Fix display way

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -9,17 +9,10 @@ class PortfolioController < ApplicationController
     :check_password_change
   ]
 
-  skip_before_filter *PRIVATE_ACCESS, :only => :index
-  before_filter :public_access?, :only => :index
-
   helper :all
 
   def index
     @projects = Project.portfolio
   end
 
-  protected
-    def public_access?
-      PRIVATE_ACCESS.map{ |v| send(v) }.all? unless Portfolio::Redmine.public_access?
-    end
 end
