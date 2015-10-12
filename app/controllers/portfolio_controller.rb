ee6273be975745be802985a20b17cfc393fcf57d
Marcello Barnaba <vjt@openssl.it>
2015-10-12 18:13:35 +0200

    Revert "Re enable anonymous mode"
    This reverts commit 3d88c55560f8905a7d131d829add6c4dfa2aa000.

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -3,24 +3,10 @@ class PortfolioController < ApplicationController
 
   layout 'portfolio'
 
-  PRIVATE_ACCESS = [
-    :user_setup,
-    :check_if_login_required,
-    :check_password_change
-  ]
-
-  skip_before_filter *PRIVATE_ACCESS, :only => :index
-  before_filter :public_access?, :only => :index
-
   helper :all
 
   def index
     @projects = Project.portfolio
   end
-
-  protected
-    def public_access?
-      PRIVATE_ACCESS.map{ |v| send(v) }.all? unless Portfolio::Redmine.public_access?
-    end
 
 end
